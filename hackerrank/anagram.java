60ede1ef6fe1909bdd12fb6ddc6c0971e8170511
Ramya-ambekar <77728237+Ramya-ambekar@users.noreply.github.com>
2021-06-23 14:12:23 +0530

    Add files via upload

---
+++ hackerrank/anagram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class anagram {
+	public static void main(String[] args){
+		String a="hello";
+		String b="Hello";
+		char[] temparray=s.toCharArray();
+		String st="";
+		int count=0;
+		for(int i=1;i<=a.length()-1;i++){
+			if(a.length()==b.length()){
+		
+			}
+			System.out.print("Not anagram");
+		}
+		
+	}
+}
